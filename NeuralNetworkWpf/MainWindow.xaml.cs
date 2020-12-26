bd8edc583f29449718917ff6881ec173b15f2f4f
Ivan Scoropad <vaneawar@gmail.com>
2020-12-26 16:14:57 +0200

    qwefdfd
    dfsdfs
    
    asdada
    
    Add project files.
    
    Add .gitignore and .gitattributes.

---
+++ NeuralNetworkWpf/MainWindow.xaml.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeuralNetworkWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NeuralNetwork Neural { get; set; }
+        string path = @"C:\Users\Ivan\Desktop\Kursach NeuralNetwork\Saves";
+        Matrix m;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Neural = new NeuralNetwork(784, 18, 18, 10, 0.2);
+            Neural.Load(path);
+            m = new Matrix();
+            Canvas.DefaultDrawingAttributes.Height = 9;
+            Canvas.DefaultDrawingAttributes.Width = 9;
+        }
+        private ImageSource MakeImage(byte[] arr)
+        {
+            Bitmap bmp = new Bitmap(28, 28);
+            int k = 0;
+            for (int i = 0; i < 28; i++)
+            {
+                for (int j = 0; j < 28; j++)
+                {
+                    bmp.SetPixel(j, i, System.Drawing.Color.FromArgb(arr[k], arr[k], arr[k]));
+                    k++;
+                }
+            }
+            return ImageSourceFromBitmap(bmp);
+        }
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool DeleteObject([In] IntPtr hObject);
+
+        private ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+               
+            }
+            finally { DeleteObject(handle); }
+        }
+        public static Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            Bitmap bitmap;
+            using (var outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+            return bitmap;
+        }
+
+        private void Train_Click(object sender, RoutedEventArgs e)
+        {
+            new Task(() => TrainMethod()).Start();
+           
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.Strokes.Clear();
+        }
+        public System.Drawing.Bitmap BitmapSourceToBitmap2(BitmapSource srs)
+        {
+            int width = srs.PixelWidth;
+            int height = srs.PixelHeight;
+            int stride = width * ((srs.Format.BitsPerPixel + 7) / 8);
+            IntPtr ptr = IntPtr.Zero;
+            try
+            {
+                ptr = Marshal.AllocHGlobal(height * stride);
+                srs.CopyPixels(new Int32Rect(0, 0, width, height), ptr, height * stride, stride);
+                using (var btm = new System.Drawing.Bitmap(width, height, stride, System.Drawing.Imaging.PixelFormat.Format1bppIndexed, ptr))
+                {
+                    // Clone the bitmap so that we can dispose it and
+                    // release the unmanaged memory at ptr
+                    return new System.Drawing.Bitmap(btm);
+                }
+            }
+            finally
+            {
+                if (ptr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(ptr);
+            }
+        }
+        private byte[] SignatureToBitmapBytes()
+        {
+            //get the dimensions of the ink control
+            int margin = (int)this.Canvas.Margin.Left;
+            int width = (int)this.Canvas.ActualWidth - margin;
+            int height = (int)this.Canvas.ActualHeight - margin;
+
+            //render ink to bitmap
+            RenderTargetBitmap rtb =
+               new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(Canvas);
+
+            //save the ink to a memory stream
+            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(rtb));
+            byte[] bitmapBytes;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+
+                //get the bitmap bytes from the memory stream
+                ms.Position = 0;
+                bitmapBytes = ms.ToArray();
+            }
+            return bitmapBytes;
+        }
+        public static Bitmap ConvertToBitmap(BitmapSource bitmapSource)
+        {
+            var width = bitmapSource.PixelWidth;
+            var height = bitmapSource.PixelHeight;
+            var stride = width * ((bitmapSource.Format.BitsPerPixel + 7) / 8);
+            var memoryBlockPointer = Marshal.AllocHGlobal(height * stride);
+            bitmapSource.CopyPixels(new Int32Rect(0, 0, width, height), memoryBlockPointer, height * stride, stride);
+            var bitmap = new Bitmap(width, height, stride, System.Drawing.Imaging.PixelFormat.Format32bppArgb, memoryBlockPointer);
+            return bitmap;
+        }
+        private System.Drawing.Bitmap ConvertInkCanvasToImage()
+        {
+
+            //render bitmap
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)Canvas.ActualWidth, (int)Canvas.ActualHeight, 96, 96, System.Windows.Media.PixelFormats.Pbgra32);
+            rtb.Render(Canvas);
+            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(rtb));
+            rtb.Render(Canvas);
+
+            //save to memory stream or file 
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                encoder.Save(ms);
+                Bitmap bitmap = new Bitmap(ms);
+                return bitmap;
+
+            }
+
+        }
+        
+        private void Query_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            Bitmap bmp = ConvertInkCanvasToImage();
+            Picture.Source = ImageSourceFromBitmap(bmp);
+            byte[] array = new byte[784];
+            int q = 0;
+            for (int i = 0; i < 140; i+=5)
+            {
+                for (int j = 0; j < 140; j += 5)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < 5; k++)
+                    {
+                        for (int z = 0; z < 5; z++)
+                        {
+                            sum += bmp.GetPixel(j + k, i + z).R;
+                        }
+                    }
+                    array[q++] = (byte)(255 - (sum / 25));
+                }
+            }
+            double[] resultArray =  Neural.Query(Normalize(array));
+            Result0.Content = string.Format("0 - {0:0.00}", resultArray[0] * 100);
+            Result1.Content = string.Format("1- {0:0.00}", resultArray[1] * 100);
+            Result2.Content = string.Format("2 - {0:0.00}", resultArray[2] * 100);
+            Result3.Content = string.Format("3 - {0:0.00}", resultArray[3] * 100);
+            Result4.Content = string.Format("4 - {0:0.00}", resultArray[4] * 100);
+            Result5.Content = string.Format("5 - {0:0.00}", resultArray[5] * 100);
+            Result6.Content = string.Format("6 - {0:0.00}", resultArray[6] * 100);
+            Result7.Content = string.Format("7 - {0:0.00}", resultArray[7] * 100);
+            Result8.Content = string.Format("8 - {0:0.00}", resultArray[8] * 100);
+            Result9.Content = string.Format("9 - {0:0.00}", resultArray[9] * 100);
+            //var result = Array.IndexOf(resultArray, resultArray.Max());
+            //Result.Content = result;
+            Picture.Source = MakeImage(array);
+
+
+        }
+        private void TrainMethod()
+        {
+            double[] array = new double[784];
+            string number;
+            using (var sr = new StreamReader(@"C:\Users\Ivan\Desktop\Kursach NeuralNetwork\mnist_train.txt"))
+            {
+                int plus = 0;
+                int minus = 0;
+                for (int j = 0; j < 100; j++)
+                {
+                    double error = 0;
+                    for (int k = 0; k < 600; k++)
+                    {
+                        double[] numbers = new double[10];
+                        //разбиваем на байты(стринги)
+                        var strarray = sr.ReadLine().Split(',');
+                        //получаем число котороые ожидаем
+                        number = strarray[0];
+                        numbers[Convert.ToInt32(number)] = 0.99;
+                        array = Normalize(strarray);
+                        Neural.Train(array, numbers);
+                        
+
+                        /*if(index == Convert.ToInt32(number))
+                        {
+                            plus++;
+                        }
+                        else
+                        {
+                            minus++;
+                        }
+                        Dispatcher.Invoke(() => Error.Content = $"{plus}/{minus}");*/
+
+                    }
+                    Neural.Save(path);
+                    Dispatcher.Invoke(() => Progress.Content = $"Progress: {j}%");
+                }
+
+            }
+            
+        }
+        private double[] Normalize(byte[] arr)
+        {
+            double[] array = new double[arr.Length];
+            for (int i = 1; i < arr.Length; i++)
+            {
+
+                //нормализуем данные, чтобы были между 0 и 1
+                array[i - 1] = Convert.ToDouble(arr[i - 1]) / 256 + 0.01;
+            }
+            return array;
+        }
+        private double[] Normalize(string[] arr)
+        {
+            double[] array = new double[arr.Length];
+            for (int i = 1; i < arr.Length; i++)
+            {
+
+                //нормализуем данные, чтобы были между 0 и 1
+                array[i - 1] = Convert.ToDouble(arr[i - 1]) / 256 + 0.01;
+            }
+            return array;
+        }
+
+        private void TestQuery_Click(object sender, RoutedEventArgs e)
+        {
+            new Task(() => TestQueryMethod()).Start();
+        }
+        private void TestQueryMethod()
+        {
+            double[] array = new double[784];
+            string number;
+            using (var sr = new StreamReader(@"C:\Users\Ivan\Desktop\Kursach NeuralNetwork\mnist_train.txt"))
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    for (int k = 0; k < 600; k++)
+                    {
+                        double[] numbers = new double[10];
+                        var strarray = sr.ReadLine().Split(',');
+                        number = strarray[0];
+
+                        array = Normalize(strarray);
+                        var resultArray = Neural.Query(array);
+                        Dispatcher.Invoke(() => Result0.Content = string.Format("0 - {0:0.00}", resultArray[0] * 100));
+                        Dispatcher.Invoke(() => Result1.Content = string.Format("1 - {0:0.00}", resultArray[1] * 100));
+                        Dispatcher.Invoke(() => Result2.Content = string.Format("2 - {0:0.00}", resultArray[2] * 100));
+                        Dispatcher.Invoke(() => Result3.Content = string.Format("3 - {0:0.00}", resultArray[3] * 100));
+                        Dispatcher.Invoke(() => Result4.Content = string.Format("4 - {0:0.00}", resultArray[4] * 100));
+                        Dispatcher.Invoke(() => Result5.Content = string.Format("5 - {0:0.00}", resultArray[5] * 100));
+                        Dispatcher.Invoke(() => Result6.Content = string.Format("6 - {0:0.00}", resultArray[6] * 100));
+                        Dispatcher.Invoke(() => Result7.Content = string.Format("7 - {0:0.00}", resultArray[7] * 100));
+                        Dispatcher.Invoke(() => Result8.Content = string.Format("8 - {0:0.00}", resultArray[8] * 100));
+                        Dispatcher.Invoke(() => Result9.Content = string.Format("9 - {0:0.00}", resultArray[9] * 100));
+                        byte[] imgArray = new byte[784];
+                        for (int h = 0; h < 784; h++)
+                        {
+                            imgArray[h] = Convert.ToByte(strarray[h + 1]);
+                        }
+                        Dispatcher.Invoke(() => Picture.Source = MakeImage(imgArray));
+                        Thread.Sleep(5000);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NeuralNetworkWpf/NeuralNetworkLib.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkWpf
+{
+    public class NeuralNetwork
+    {
+        int iNodes;
+        int oNodes;
+        int hNodes;
+        int hNodes2;
+        double lr;
+        double[,] wih;
+        double[,] wih2;
+        double[,] who;
+        Matrix m;
+        double[] hidden_inputs;
+        double[] hidden_outputs;
+        double[] hidden_inputs2;
+        double[] hidden_outputs2;
+        double[] final_inputs;
+        public double[] final_outputs;
+        double[] inputs;
+        double[] target;
+        public double[] output_errors;
+        double[] final;
+        public double[] hidden_errors;
+        double[] hidden_errors2;
+        public NeuralNetwork(int inodes, int hnodes, int hnodes2, int outnodes, double learningRate)
+        {
+
+            iNodes = inodes;
+            hNodes = hnodes;
+            oNodes = outnodes;
+            hNodes2 = hnodes2;
+            lr = learningRate;
+            m = new Matrix();
+            wih = new double[iNodes, hNodes];
+            wih2 = new double[hNodes, hNodes2];
+            who = new double[hNodes2, oNodes];
+            wih = m.rand(iNodes, hNodes);
+            wih2 = m.rand(hNodes, hNodes2);
+            who = m.rand(hNodes2, oNodes);
+            hidden_inputs = new double[hNodes];
+            hidden_outputs = new double[hNodes];
+            hidden_inputs2 = new double[hNodes2];
+            hidden_outputs2 = new double[hNodes2];
+            final_inputs = new double[oNodes];
+            final_outputs = new double[oNodes];
+            inputs = new double[iNodes];
+        }
+        public void Train(double[] inputs_list, double[] target_list)
+        {
+            inputs = inputs_list;
+            target = target_list;
+            hidden_inputs = m.GetX(wih, inputs);
+            hidden_outputs = Activation(hidden_inputs);
+            hidden_inputs2 = m.GetX(wih2, hidden_outputs);
+            hidden_outputs2 = Activation(hidden_inputs2);
+            final_inputs = m.GetX(who, hidden_outputs2);
+            final_outputs = Activation(final_inputs);
+            output_errors = m.Minus(target, final_outputs);
+            hidden_errors2 = m.BackMul(who, output_errors);
+            hidden_errors = m.BackMul(wih2, hidden_errors2);
+            who = m.Plus(who, m.DeltaWeights(output_errors, who, hidden_outputs2, lr));
+            wih2 = m.Plus(wih2, m.DeltaWeights(hidden_errors2, wih2, hidden_outputs, lr));
+            wih = m.Plus(wih, m.DeltaWeights(hidden_errors, wih, inputs, lr));
+        }
+        public double[] Query(double[] inputs_list)
+        {
+            inputs = inputs_list;
+            hidden_inputs = m.GetX(wih, inputs);
+            hidden_outputs = Activation(hidden_inputs);
+            hidden_inputs2 = m.GetX(wih2, hidden_outputs);
+            hidden_outputs2 = Activation(hidden_inputs2);
+            final_inputs = m.GetX(who, hidden_outputs2);
+            final_outputs = Activation(final_inputs);
+            return final_outputs;
+
+        }
+        double[] Activation(double[] x)
+        {
+            return m.Sigmoid(x);
+        }
+        public void Save(string path)
+        {
+
+            string WHO = path + "\\WHO.txt";
+            string WIH2 = path + "\\WIH2.txt";
+            string WIH = path + "\\WIH.txt";
+            using (StreamWriter save = new StreamWriter(WHO, false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < who.GetLength(0); i++)
+                {
+                    for (int j = 0; j < who.GetLength(1); j++)
+                    {
+                        save.Write(who[i, j] + " ");
+                    }
+                    save.WriteLine();
+                }
+            }
+            using (StreamWriter save = new StreamWriter(WIH2, false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < wih2.GetLength(0); i++)
+                {
+                    for (int j = 0; j < wih2.GetLength(1); j++)
+                    {
+                        save.Write(wih2[i, j] + " ");
+                    }
+                    save.WriteLine();
+                }
+            }
+            using (StreamWriter save = new StreamWriter(WIH, false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < wih.GetLength(0); i++)
+                {
+                    for (int j = 0; j < wih.GetLength(1); j++)
+                    {
+                        save.Write(wih[i, j] + " ");
+                    }
+                    save.WriteLine();
+                }
+            }
+        }
+        public void Load(string path)
+        {
+            string WHO = path + "\\WHO.txt";
+            string WIH2 = path + "\\WIH2.txt";
+            string WIH = path + "\\WIH.txt";
+            char split = ' ';
+            using (StreamReader load = new StreamReader(WHO, System.Text.Encoding.Default))
+            {
+                string[] local = new string[who.GetLength(1)];
+                for (int i = 0; i < who.GetLength(0); i++)
+                {
+
+                    local = load.ReadLine().Split(split);
+                    for (int k = 0; k < who.GetLength(1); k++)
+                    {
+                        who[i, k] = Convert.ToDouble(local[k]);
+                    }
+
+                }
+            }
+            using (StreamReader load = new StreamReader(WIH2, System.Text.Encoding.Default))
+            {
+                string[] local = new string[wih2.GetLength(1)];
+                for (int i = 0; i < wih2.GetLength(0); i++)
+                {
+
+                    local = load.ReadLine().Split(split);
+                    for (int k = 0; k < wih2.GetLength(1); k++)
+                    {
+                        wih2[i, k] = Convert.ToDouble(local[k]);
+                    }
+
+                }
+            }
+            using (StreamReader load = new StreamReader(WIH, System.Text.Encoding.Default))
+            {
+                string[] local = new string[wih.GetLength(1)];
+                for (int i = 0; i < wih.GetLength(0); i++)
+                {
+
+                    local = load.ReadLine().Split(split);
+                    for (int k = 0; k < wih.GetLength(1); k++)
+                    {
+                        wih[i, k] = Convert.ToDouble(local[k]);
+                    }
+
+                }
+            }
+        }
+    }
+    class Matrix
+    {
+        public double[] GetX(double[,] weight, double[] input, double bias = 0)
+        {
+            int len1 = weight.GetLength(0);
+            int len2 = weight.GetLength(1);
+
+            double[] x = new double[len2];
+            for (int i = 0; i < len1; i++)
+            {
+                for (int j = 0; j < len2; j++)
+                {
+                    x[j] += weight[i, j] * input[i];
+                    x[j] += bias;
+                }
+
+            }
+            return x;
+        }
+        public double[] Sigmoid(double[] input)
+        {
+            int len = input.Length;
+            double[] sig = new double[len];
+            for (int i = 0; i < len; i++)
+            {
+                sig[i] = 1 / (1 + Math.Exp(-input[i]));
+            }
+            return sig;
+        }
+        public double[,] Sigmoid(double[,] input)
+        {
+
+            double[,] sig = new double[input.GetLength(0), input.GetLength(1)];
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    sig[i, j] = 1 / (1 + Math.Exp(-input[i, j]));
+                }
+
+            }
+            return sig;
+        }
+        public double[,] Normalize(double num, double[,] weight)
+        {
+
+            double sum = 0;
+            //double sred = 0;
+            for (int i = 0; i < weight.GetLength(0); i++)
+            {
+                for (int j = 0; j < weight.GetLength(1); j++)
+                {
+                    sum += weight[i, j];
+                }
+            }
+            num += sum / weight.Length;
+            for (int i = 0; i < weight.GetLength(0); i++)
+            {
+                for (int j = 0; j < weight.GetLength(1); j++)
+                {
+                    weight[i, j] -= num;
+                }
+            }
+            return weight;
+        }
+        public double[,] rand(int rows, int columns)
+        {
+            Random r = new Random();
+            double[,] ran = new double[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    ran[i, j] = r.NextDouble() - 0.5;
+                }
+            }
+            return ran;
+        }
+        public double[,] T(double[,] input)
+        {
+            double[,] local = new double[input.GetLength(1), input.GetLength(0)];
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    local[j, i] = input[i, j];
+                }
+            }
+            return local;
+        }
+        public double[] BasicMul(double[] first, double[] second)
+        {
+            double[] final = new double[second.Length];
+            for (int i = 0; i < second.Length; i++)
+            {
+                final[i] = first[i] * second[i];
+            }
+            return final;
+        }
+        public double[,] T(double[] input)
+        {
+            double[,] local = new double[input.Length, 1];
+            for (int i = 0; i < input.Length; i++)
+            {
+                local[i, 0] = input[i];
+            }
+            return local;
+        }
+        public double[] BackT(double[,] input)
+        {
+            double[] local = new double[input.GetLength(0)];
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                local[i] = input[i, 0];
+            }
+            return local;
+        }
+        public double[] Minus(double[] first, double[] second)
+        {
+            double[] final = new double[second.Length];
+            for (int i = 0; i < second.Length; i++)
+            {
+                final[i] = first[i] - second[i];
+            }
+            return final;
+        }
+        public double[,] Plus(double[,] weights, double[,] delta)
+        {
+            for (int i = 0; i < weights.GetLength(0); i++)
+            {
+                for (int j = 0; j < weights.GetLength(1); j++)
+                {
+                    weights[i, j] += delta[i, j];
+                }
+            }
+            return weights;
+        }
+        public double[,] MulVectors(double[] first, double[] second)
+        {
+            double[,] local = new double[first.Length, second.Length];
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < second.Length; j++)
+                {
+                    local[i, j] = first[i] * second[j];
+                }
+            }
+            return local;
+        }
+        public double[] Plus(double[] first, double[] second)
+        {
+            double[] final = new double[second.Length];
+            for (int i = 0; i < second.Length; i++)
+            {
+                final[i] = first[i] + second[i];
+            }
+            return final;
+        }
+
+        public double[] BackMul(double[,] first, double[] second)
+        {
+            double[] local = new double[first.GetLength(0)];
+            for (int i = 0; i < first.GetLength(0); i++)
+            {
+                for (int j = 0; j < first.GetLength(1); j++)
+                {
+
+                    local[i] += first[i, j] * second[j];
+
+
+                }
+            }
+            return local;
+        }
+        public double[,] DeltaWeights(double[] errors, double[,] weights, double[] outputs, double lr)
+        {
+            double[,] summa = new double[weights.GetLength(1), weights.GetLength(0)];
+            for (int k = 0; k < weights.GetLength(0); k++)
+            {
+                for (int i = 0; i < weights.GetLength(0); i++)
+                {
+                    for (int j = 0; j < weights.GetLength(1); j++)
+                    {
+                        summa[j, i] += weights[k, j] * outputs[k];
+                    }
+                }
+            }
+            double[,] sig = new double[summa.GetLength(0), summa.GetLength(1)]; //Такой размер чтобы вставить Т матрицу
+            sig = Sigmoid(T(summa));
+
+            for (int i = 0; i < sig.GetLength(0); i++)
+            {
+                for (int j = 0; j < sig.GetLength(1); j++)
+                {
+                    sig[i, j] -= sig[i, j] * sig[i, j];
+                }
+            }
+
+            double[,] final = new double[sig.GetLength(0), sig.GetLength(1)];
+            for (int i = 0; i < final.GetLength(1); i++)
+            {
+                for (int j = 0; j < final.GetLength(0); j++)
+                {
+                    final[j, i] = lr * errors[i] * sig[j, i] * outputs[j];
+                }
+            }
+            return final;
+        }
+    }
+}
